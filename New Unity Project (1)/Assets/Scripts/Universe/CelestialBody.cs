f7f9b3e77840a77044f5844b0643b560bc0a9d0b
bitegw <bitegw@gmail.com>
2020-11-11 21:17:05 +0100

    Added solar system gravity.

---
+++ New Unity Project (1)/Assets/Scripts/Universe/CelestialBody.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Universe
+{
+    public class CelestialBody : MonoBehaviour
+    {
+        [SerializeField] public float surfaceGravity;
+        [SerializeField] public float radius;
+        [SerializeField] public Vector3 initialVelocity;
+        [SerializeField] public Color trailColor;
+
+        public float Mass
+        {
+            get
+            {
+                return surfaceGravity * radius * radius / UniverseConstants.G;
+            }
+        }
+    }
+}
