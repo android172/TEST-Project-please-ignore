1bb84948723827721eba04457550f8c0f898638a
Litinum <zucamunja@gmail.com>
2020-12-01 10:51:24 +0100

    Water Shader
    Added reflection, alpha offset

---
+++ TEST Project please ignore/Assets/Scripts/UnderwaterCam.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnderwaterCam : MonoBehaviour
+{
+    public float waterHeight;
+
+    private bool isUnderwater;
+    private Color normalCol;
+    private Color uwCol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        normalCol = new Color (0.5f, 0.5f, 0.5f, 0.5f);
+        uwCol = new Color (0.22f, 0.65f, 0.77f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((transform.position.y < waterHeight) != isUnderwater)
+        {
+            isUnderwater = transform.position.y < waterHeight;
+            if(isUnderwater)
+            {
+                RenderSettings.fogColor = normalCol;
+                RenderSettings.fogDensity = 0.01f;
+            }
+            if(!isUnderwater)
+            {
+                RenderSettings.fogColor = uwCol;
+                RenderSettings.fogDensity = 0.1f;
+            }
+        }
+    }
+}
