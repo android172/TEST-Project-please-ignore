05b6543bcf3ee49d4d264ecb22b2892352412d3f
bitegw <bitegw@gmail.com>
2020-12-06 03:58:09 +0100

    Migrated shader from previous version. Updated gravity and added debug display.

--- TEST Project please ignore/Assets/Scripts/Universe/CelestialBody.cs
+++ TEST Project please ignore/Assets/Scripts/Universe/CelestialBody.cs
@@ -11,12 +11,46 @@ namespace Universe
         [SerializeField] public Vector3 initialVelocity;
         [SerializeField] public Color trailColor;
 
+        private Vector3 velocity;
+
         public float Mass
         {
             get
             {
                 return surfaceGravity * radius * radius / UniverseConstants.G;
             }
+        }
+
+        private void Awake()
+        {
+            velocity = initialVelocity;
+        }
+
+        public void UpdateVelocity(CelestialBody[] allBodies, float timeStep)
+        {
+            foreach (var otherBody in allBodies)
+            {
+                if (otherBody != this)
+                {
+                    float sqrDst = (otherBody.transform.position - transform.position).sqrMagnitude;
+                    Vector3 forceDir = (otherBody.transform.position - transform.position).normalized;
+
+                    Vector3 acceleration = forceDir * UniverseConstants.G * otherBody.Mass / sqrDst;
+                    velocity += acceleration * timeStep;
+                }
+            }
+        }
+
+        public void UpdateVelocity(Vector3 acceleration, float timeStep)
+        {
+            velocity += acceleration * timeStep;
+        }
+
+        public void UpdatePosition(float timeStep)
+        {
+            //rb.MovePosition(rb.position + velocity * timeStep);
+            transform.position += velocity * timeStep;
+
         }
     }
 }
