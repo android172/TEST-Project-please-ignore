2fdc78650ab9e9dd743a20fce37a9fb1d48afd7d
Danijel Andjelkovic <adanijel99@gmail.com>
2021-05-13 10:05:16 +0200

    Fixed a bug with atmosphere. Created player prefab with gravity.

--- TEST Project please ignore/Assets/Scripts/PlayerGravity.cs
+++ TEST Project please ignore/Assets/Scripts/PlayerGravity.cs
@@ -4,35 +4,26 @@ using UnityEngine;
 using Universe;
 
 [RequireComponent(typeof(Rigidbody))]
-public class Player : MonoBehaviour
+public class PlayerGravity : MonoBehaviour
 {
     public LayerMask groundMask;
     public float downRayDist = 0.5f;
 
-    [Header("Camera:")]
-    public float sensitivityHorizontal = 1.0f;
-    public float sensitivityVertical = 1.0f;
-    public float angleMin = -80f, angleMax = 90f;
-
-    [Header("Movement:")]
-    public float speed = 10f;
     public float stickToGroundForce = 0.1f;
 
-    private Vector3 desiredVelocity;
     private Vector3 gravity;
-
-    private Vector2 rotation;
 
     private SolarSystem ss;
     private Rigidbody rb;
-    private Camera cam;
 
     private bool grounded;
+
+    public float jetPackSpeed;
+    public float jumpPower;
 
     // Start is called before the first frame update
     void Start()
     {
-        cam = Camera.main;
         ss = FindObjectOfType<SolarSystem>();
         rb = GetComponent<Rigidbody>();
         rb.useGravity = false;
@@ -43,31 +34,45 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Rotate
-        Vector3 up = (-gravity).normalized;
+        //rb.MovePosition(rb.position + (grounded ? gravity.normalized * stickToGroundForce : gravity) * Time.deltaTime);
 
-        rotation.y += Input.GetAxis("Mouse X");
-        rotation.x -= Input.GetAxis("Mouse Y");
-        rotation.x = Mathf.Clamp(rotation.x, angleMin, angleMax);
+        Vector3 up = (-(gravity.normalized));
 
-        transform.rotation = Quaternion.AngleAxis(rotation.y * sensitivityHorizontal, up);
-        cam.transform.localEulerAngles = new Vector2(rotation.x * sensitivityVertical, 0);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += up * jetPackSpeed * Time.deltaTime;
+        }
 
-        //Move
-        var input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
-        desiredVelocity = transform.up * input.z + transform.right * input.x;
-
-        var velocity = desiredVelocity * speed + (grounded ? gravity.normalized * stickToGroundForce : gravity);
-        rb.MovePosition(rb.position + velocity * Time.deltaTime);
+        if (grounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("Hop");
+                rb.AddForce(up * jumpPower);
+                grounded = false;
+            }
+            else
+            {
+                rb.velocity += gravity.normalized * stickToGroundForce * Time.deltaTime;
+            }
+        }
+        else
+        {
+            rb.velocity += gravity * Time.deltaTime;
+        }
     }
 
     private void FixedUpdate()
     {
         gravity = ss.CalculateVelocity(rb.position, gravity, Time.fixedDeltaTime);
+    }
 
+    private void OnCollisionEnter(Collision collision)
+    {
         //Check ground
         Ray downRay = new Ray(rb.position, gravity);
         Vector3 dRay = gravity.normalized * downRayDist;
+
         if (Physics.Raycast(downRay, maxDistance: downRayDist, layerMask: groundMask))
         {
             gravity = Vector3.zero;
